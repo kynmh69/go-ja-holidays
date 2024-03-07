435dbacdba81613eeb1b23b11cb3d8d19ae3cb70
kynmh69 <hiroki.kayanuma@gmail.com>
2024-03-07 19:59:12 +0900

    feature add database updater #1 add create struct

--- src/updater/main.go
+++ src/updater/main.go
@@ -1,9 +1,14 @@
 package main
 
-import "github.com/kynmh69/go-ja-holidays/util"
+import (
+	"log"
+
+	"github.com/kynmh69/go-ja-holidays/util"
+)
 
 func main() {
 	url := "https://www8.cao.go.jp/chosei/shukujitsu/syukujitsu.csv"
 
-	util.CreateHolidayData(url)
+	holidays := util.CreateHolidayData(url)
+	log.Println(holidays)
 }
