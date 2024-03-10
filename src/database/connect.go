218d0c8304428b2db760d7658c4bc7bf757c1fa5
kynmh69 <hiroki.kayanuma@gmail.com>
2024-03-10 17:41:28 +0900

    feature add to get holidays #2 create test.

--- src/database/connect.go
+++ src/database/connect.go
@@ -20,10 +20,7 @@ const DATABASE_NAME = "holidays"
 func ConnectDatabase() {
 	// connect to database
 	var err error
-
-	hostname, port, username, password := getConnectionInfo()
-
-	dataSourceName := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable", hostname, port, username, password, DATABASE_NAME)
+	hostname, port, dataSourceName := CreateConnectInfo()
 
 	db, err := sql.Open("postgres", dataSourceName)
 	if err != nil {
@@ -43,24 +40,37 @@ func ConnectDatabase() {
 	goquDb.Logger(initLogger())
 }
 
-func getConnectionInfo() (string, string, string, string) {
+func CreateConnectInfo() (string, string, string) {
+
+	hostname, port, username, password, databaseName := getConnectionInfo()
+
+	dataSourceName := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable", hostname, port, username, password, databaseName)
+	return hostname, port, dataSourceName
+}
+
+func getConnectionInfo() (hostname, port, username, password, databaseName string) {
 	hostname, ok := os.LookupEnv("PSQL_HOSTNAME")
 	if !ok {
 		hostname = "database"
 	}
-	port, ok := os.LookupEnv("PSQL_PORT")
+	port, ok = os.LookupEnv("PSQL_PORT")
 	if !ok {
 		port = "5432"
 	}
-	username, ok := os.LookupEnv("PSQL_USERNAME")
+	username, ok = os.LookupEnv("PSQL_USERNAME")
 	if !ok {
 		username = "app"
 	}
-	password, ok := os.LookupEnv("PSQL_PASSWORD")
+	password, ok = os.LookupEnv("PSQL_PASSWORD")
 	if !ok {
 		password = "password"
 	}
-	return hostname, port, username, password
+	databaseName, ok = os.LookupEnv("DATABASE")
+
+	if !ok {
+		databaseName = DATABASE_NAME
+	}
+	return
 }
 
 func GetDbConnection() *goqu.Database {
---
+++ src/updater/controller/save_holidays_test.go
@@ -0,0 +1,206 @@
+package controller
+
+import (
+	"log"
+	"os"
+	"reflect"
+	"testing"
+	"time"
+
+	"github.com/kynmh69/go-ja-holidays/database"
+)
+
+func TestMain(m *testing.M) {
+	setUp()
+	defer tearDown()
+	exitVal := m.Run()
+	os.Exit(exitVal)
+}
+
+func TestSaveHolidays(t *testing.T) {
+	loc, err := time.LoadLocation("Asia/Tokyo")
+	if err != nil {
+		log.Println(err)
+	}
+	holidays := []HolidayDbData{
+		{
+			Date: time.Date(2004, 1, 4, 0, 0, 0, 0, loc),
+			Name: "テスト祝日4",
+		},
+	}
+	type args struct {
+		holidays []HolidayDbData
+	}
+	tests := []struct {
+		name string
+		args args
+	}{
+		{
+			name: "ok",
+			args: args{
+				holidays: holidays,
+			},
+		},
+		{
+			name: "ok new submit",
+			args: args{
+				holidays: holidays,
+			},
+		},
+	}
+	for i, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			switch i {
+			case 1:
+				db := database.GetDbConnection()
+				db.Delete(TABLE_HOLIDAYS_JP)
+			}
+			SaveHolidays(tt.args.holidays)
+		})
+	}
+}
+
+func Test_getLatestHoliday(t *testing.T) {
+	wants, _ := getLatestHoliday()
+	tests := []struct {
+		name  string
+		want  HolidayDbData
+		want1 bool
+	}{
+		{
+			name: "ok",
+			want: wants,
+			want1: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, got1 := getLatestHoliday()
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("getLatestHoliday() got = %v, want %v", got, tt.want)
+			}
+			if got1 != tt.want1 {
+				t.Errorf("getLatestHoliday() got1 = %v, want %v", got1, tt.want1)
+			}
+		})
+	}
+}
+
+func Test_firstInsertHolidays(t *testing.T) {
+	loc, err := time.LoadLocation("Asia/Tokyo")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	holidaysD := []HolidayDbData{
+		{Date: time.Date(2002, 1, 1, 0, 0, 0, 0, loc), Name: "テスト祝日1"},
+		{Date: time.Date(2002, 1, 2, 0, 0, 0, 0, loc), Name: "テスト祝日2"},
+		{Date: time.Date(2002, 1, 3, 0, 0, 0, 0, loc), Name: "テスト祝日3"},
+		{Date: time.Date(2002, 1, 4, 0, 0, 0, 0, loc), Name: "テスト祝日4"},
+	}
+	type args struct {
+		holidays []HolidayDbData
+	}
+	tests := []struct {
+		name string
+		args args
+	}{
+		{
+			name: "ok",
+			args: args{holidaysD},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			firstInsertHolidays(tt.args.holidays)
+		})
+	}
+}
+
+func Test_createDiff(t *testing.T) {
+	newHolidayData := []HolidayDbData{
+		{Date: time.Now().AddDate(1, 0, 0), Name: "テスト祝日"},
+	}
+	oldData := HolidayDbData{Date: time.Now(), Name: "テスト祝日最後"}
+	type args struct {
+		newHolidayData []HolidayDbData
+		oldHolidayData HolidayDbData
+	}
+	tests := []struct {
+		name string
+		args args
+		want []HolidayDbData
+	}{
+		{
+			name: "ok",
+			args: args{
+				newHolidayData: newHolidayData,
+				oldHolidayData: oldData,
+			},
+			want: newHolidayData,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := createDiff(tt.args.newHolidayData, tt.args.oldHolidayData); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("createDiff() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+	tearDown()
+}
+
+func Test_updateData(t *testing.T) {
+	newHolidayData := []HolidayDbData{
+		{Date: time.Now().AddDate(1, 0, 0), Name: "テスト祝日"},
+	}
+	oldData := HolidayDbData{Date: time.Now(), Name: "テスト祝日最後"}
+	type args struct {
+		newHolidayData []HolidayDbData
+		oldData        HolidayDbData
+	}
+	tests := []struct {
+		name string
+		args args
+	}{
+		{
+			"ok",
+			args{
+				newHolidayData: newHolidayData,
+				oldData:        oldData,
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			updateData(tt.args.newHolidayData, tt.args.oldData)
+		})
+	}
+	tearDown()
+}
+
+func setUp() {
+	os.Setenv("DATABASE", "unittest")
+	os.Setenv("PSQL_HOSTNAME", "localhost")
+	loc, err := time.LoadLocation("Asia/Tokyo")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	holidays := []HolidayDbData{
+		{Date: time.Date(2004, 1, 1, 0, 0, 0, 0, loc), Name: "テスト祝日1"},
+		{Date: time.Date(2004, 1, 2, 0, 0, 0, 0, loc), Name: "テスト祝日2"},
+		{Date: time.Date(2004, 1, 3, 0, 0, 0, 0, loc), Name: "テスト祝日3"},
+		{Date: time.Date(2004, 1, 4, 0, 0, 0, 0, loc), Name: "テスト祝日4"},
+	}
+	database.ConnectDatabase()
+	firstInsertHolidays(holidays)
+}
+
+func tearDown() {
+	db := database.GetDbConnection()
+	if _, err := db.Delete(TABLE_HOLIDAYS_JP).Executor().Exec(); err != nil {
+		log.Fatalln(err)
+	}
+	os.Unsetenv("DATABASE")
+	os.Unsetenv("PSQL_HOSTNAME")
+	log.Println("Tear down.")
+}
