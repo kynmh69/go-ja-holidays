a180e44b69d844862574c64a976ce0bfee979e58
kynmh69 <hiroki.kayanuma@gmail.com>
2024-03-20 09:58:31 +0900

    feature add count holidays #12 mod test

--- src/api/controller/count_holidays_test.go
+++ src/api/controller/count_holidays_test.go
@@ -78,6 +78,40 @@ func TestCountHolidaysStartDay(t *testing.T) {
 	}
 }
 
+func TestCountHolidaysEndDay(t *testing.T) {
+	e := echo.New()
+	q := make(url.Values)
+	q.Set("end-day", "2022-01-01")
+	req := httptest.NewRequest(http.MethodGet, "/holidays/count?"+q.Encode(), nil)
+	req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+	rec := httptest.NewRecorder()
+	c := e.NewContext(req, rec)
+
+	type args struct {
+		c echo.Context
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantErr bool
+	}{
+		{
+			name: "test OK",
+			args: args{
+				c: c,
+			},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if err := CountHolidays(tt.args.c); (err != nil) != tt.wantErr {
+				t.Errorf("CountHolidays() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
+
 func TestCountHolidaysStartDayErr(t *testing.T) {
 	e := echo.New()
 	q := make(url.Values)
