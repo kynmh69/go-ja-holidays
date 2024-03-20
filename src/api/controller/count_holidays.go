1b698b921a3511e519c36ca02d87d2dfcb018cfe
kynmh69 <hiroki.kayanuma@gmail.com>
2024-03-20 09:01:16 +0900

    feature add count holidays #12 add count

---
+++ src/api/controller/count_holidays.go
@@ -0,0 +1,56 @@
+package controller
+
+import (
+	"net/http"
+	"time"
+
+	"github.com/doug-martin/goqu/v9"
+	"github.com/kynmh69/go-ja-holidays/database"
+	"github.com/labstack/echo/v4"
+)
+
+type CountStruct struct {
+	Count int64
+}
+
+func CountHolidays(c echo.Context) error {
+	if location, err := time.LoadLocation(LOCATION); err != nil {
+		return BadRequestJson(c, err.Error())
+	} else {
+		goqu.SetTimeLocation(location)
+	}
+
+	startDay, err := getStartDate(c)
+	if err != nil {
+		return BadRequestJson(c, err.Error())
+	}
+
+	endDay, err := getEndDate(c)
+	if err != nil {
+		return BadRequestJson(c, err.Error())
+	}
+
+	db := database.GetDbConnection()
+
+	dataSet := db.From(TABLE_HOLIDAYS_JP)
+	if startDay != nil && endDay != nil {
+		dataSet.Where(
+			goqu.C(COLUMN_DATE).Gte(startDay),
+			goqu.C(COLUMN_DATE).Lte(endDay),
+		)
+	} else if startDay != nil {
+		dataSet.Where(
+			goqu.C(COLUMN_DATE).Gte(startDay),
+		)
+	} else if endDay != nil {
+		dataSet = dataSet.Where(
+			goqu.C(COLUMN_DATE).Lte(endDay),
+		)
+	}
+	count, err := dataSet.Count()
+	if err != nil {
+		return BadRequestJson(c, err.Error())
+	}
+	stru := CountStruct{Count: count}
+	return c.JSON(http.StatusOK, stru)
+}
